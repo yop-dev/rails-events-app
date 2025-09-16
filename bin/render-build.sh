1e9625cb601324a1426346d468e0dc812b55671e
yop-dev <desilvajoner99@gmail.com>
2025-09-16 12:24:28 +0800

    Fix build script shebang and line endings

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-# exit on error
+#!/usr/bin/env bash
 set -o errexit
 
-# Install gems
 bundle install
-
-# Precompile assets (Rails 8 with Propshaft)
 bundle exec rails assets:precompile
-
-# Run database migrations
 bundle exec rails db:migrate
